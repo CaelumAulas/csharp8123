48378ea024026f184e55d40545e9e4ad485ab828
Lucas Felix <lfelixsampaio@gmail.com>
2018-11-22 10:48:08 -0200

    arrays

---
+++ Banco-arrays-idades-contas/Banco/Conta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Banco
+{
+    internal class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; protected set; }
+        public Cliente Titular { get; set; }
+
+        public virtual void Deposita(double valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public virtual bool Saca(double valor)
+        {
+            if(valor > Saldo)
+            {
+                return false;
+            }
+            else
+            {
+                Saldo = Saldo - valor;
+                return true;
+            }
+        }
+    }
+}
---
+++ Banco-arrays-idades-contas/Banco/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Banco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textoTitular = new System.Windows.Forms.TextBox();
+            this.textoNumero = new System.Windows.Forms.TextBox();
+            this.textoSaldo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.botaoDeposito = new System.Windows.Forms.Button();
+            this.textoValor = new System.Windows.Forms.TextBox();
+            this.botaoSaque = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textoTitular
+            // 
+            this.textoTitular.Location = new System.Drawing.Point(105, 12);
+            this.textoTitular.Name = "textoTitular";
+            this.textoTitular.Size = new System.Drawing.Size(130, 22);
+            this.textoTitular.TabIndex = 0;
+            // 
+            // textoNumero
+            // 
+            this.textoNumero.Location = new System.Drawing.Point(105, 52);
+            this.textoNumero.Name = "textoNumero";
+            this.textoNumero.Size = new System.Drawing.Size(130, 22);
+            this.textoNumero.TabIndex = 1;
+            // 
+            // textoSaldo
+            // 
+            this.textoSaldo.Location = new System.Drawing.Point(105, 90);
+            this.textoSaldo.Name = "textoSaldo";
+            this.textoSaldo.Size = new System.Drawing.Size(130, 22);
+            this.textoSaldo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Titular";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Numero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Saldo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Valor";
+            // 
+            // botaoDeposito
+            // 
+            this.botaoDeposito.Location = new System.Drawing.Point(57, 178);
+            this.botaoDeposito.Name = "botaoDeposito";
+            this.botaoDeposito.Size = new System.Drawing.Size(85, 36);
+            this.botaoDeposito.TabIndex = 8;
+            this.botaoDeposito.Text = "Depositar";
+            this.botaoDeposito.UseVisualStyleBackColor = true;
+            this.botaoDeposito.Click += new System.EventHandler(this.botaoDeposito_Click);
+            // 
+            // textoValor
+            // 
+            this.textoValor.Location = new System.Drawing.Point(105, 128);
+            this.textoValor.Name = "textoValor";
+            this.textoValor.Size = new System.Drawing.Size(130, 22);
+            this.textoValor.TabIndex = 3;
+            // 
+            // botaoSaque
+            // 
+            this.botaoSaque.Location = new System.Drawing.Point(149, 178);
+            this.botaoSaque.Name = "botaoSaque";
+            this.botaoSaque.Size = new System.Drawing.Size(86, 36);
+            this.botaoSaque.TabIndex = 9;
+            this.botaoSaque.Text = "Sacar";
+            this.botaoSaque.UseVisualStyleBackColor = true;
+            this.botaoSaque.Click += new System.EventHandler(this.botaoSaque_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "media idades";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 35);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "array de contas";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 332);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.botaoSaque);
+            this.Controls.Add(this.botaoDeposito);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textoValor);
+            this.Controls.Add(this.textoSaldo);
+            this.Controls.Add(this.textoNumero);
+            this.Controls.Add(this.textoTitular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textoTitular;
+        private System.Windows.Forms.TextBox textoNumero;
+        private System.Windows.Forms.TextBox textoSaldo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button botaoDeposito;
+        private System.Windows.Forms.TextBox textoValor;
+        private System.Windows.Forms.Button botaoSaque;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
