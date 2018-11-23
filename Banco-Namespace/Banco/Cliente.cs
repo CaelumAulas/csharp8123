046e229417840b7adf287a8bb68b32f58c85414c
Lucas Felix <lfelixsampaio@gmail.com>
2018-11-23 14:38:13 -0200

    namespace

---
+++ Banco-Namespace/Banco/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace Caelum.Banco.Clientes
+{
+    internal class Cliente
+    {
+
+        public string Nome { get; set; }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
---
+++ Banco-Namespace/Banco/SaldoInsuficienteException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Banco
+{
+    [Serializable]
+    internal class SaldoInsuficienteException : Exception
+    {
+        public SaldoInsuficienteException()
+        {
+        }
+
+        public SaldoInsuficienteException(string message) : base(message)
+        {
+        }
+
+        public SaldoInsuficienteException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SaldoInsuficienteException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
